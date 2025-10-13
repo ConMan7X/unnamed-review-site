c02e2ab2c313bd0ee5877c4577ec2036a309b9f8
Connor Schicht <connor.schicht@gmail.com>
2025-10-14 10:00:08 +1100

    fix: review text newlines not showing

--- src/app/reviews/[uuid]/page.tsx
+++ src/app/reviews/[uuid]/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { supabase } from "../../../utils/supabaseClient";
 
 interface ReviewPageProps {
@@ -20,14 +21,17 @@ export default async function ReviewPage({ params }: ReviewPageProps) {
   }
 
   return (
-    <main className="flex flex-col items-center p-8">
+    <main className="flex flex-col items-center py-10 px-100">
       <h1 className="text-3xl font-bold">{post.restaurant}</h1>
-      <p className="text-gray-600 white-space-pre-line mt-4">{post.review}</p>
       {post.created_at && (
-        <p className="text-sm text-gray-400 mt-4">
+        <p className="text-sm text-teal-400 mt-4">
           Reviewed on {new Date(post.created_at).toLocaleDateString()}
         </p>
       )}
+      <p className="text-teal-400 whitespace-pre-line mt-4">{post.review}</p>
+      <Link href={`/`} className="hover:bg-gray-700">
+        Back to Home
+      </Link>
     </main>
   );
 }
